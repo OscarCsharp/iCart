36583158bb93b563257a6c08834f12c92d80f6a0
OscarCsharp <Oscarndlovu01@gmail.com>
2020-08-27 15:46:11 +0100

    Initial Sync
    First Sync

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iCart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
